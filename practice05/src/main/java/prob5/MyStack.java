c0ff1e36d194778ea3e2cae23e20ebb6035e3726
YoonHan0 <dbsgksdud12@naver.com>
2022-12-26 10:17:27 +0900

    Object로도 사용이 가능함, casting

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,28 +1,28 @@
 package prob5;
 
 public class MyStack {
-	private String[] arr;
-	int index = -1;
+	private Object[] arr;
+	int index;
 	
 	public MyStack(int size) {
+		index = -1;
 		arr = new String[size];	
 	}
 	
-	public void push(String value) {
+	public void push(Object value) {
 		index++;
 		arr[index] = value;
 	}
 	
-	public String pop() {
+	public Object pop() {
 		if(index < 0) throw new MyStackException();
-		String popData = arr[index];
+		Object popData = arr[index];
 		index--;
 		return popData;
 	}
 	
 	public boolean isEmpty() {
-		if(index < 0) return true;
-		else return false;
+		return index < 0;	// 해당 식이 참이면 true / 거짓이면 false를 리턴
 	}
 	
 	
