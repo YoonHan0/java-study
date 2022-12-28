6b75038cd72fca0894703f41d7137c285535f928
YoonHan0 <dbsgksdud12@naver.com>
2022-12-28 17:25:54 +0900

    NSLookup #finish

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,22 +2,29 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
-
+	
 	public static void main(String[] args) {
-		// 사용하는 메서드
-		// String line = "www.douzone.com";  // 입력을 이렇게 받아라~
-		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName("www.douzone.com"); 
-			for(InetAddress i : inetAddresses) {
-				System.out.println(i);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("> ");
+		while(true) {
+			String line = sc.nextLine();
+			if(line.equals("exit")) {
+				break;
 			}
-			// System.out.println(inetAddresses);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}	
-
+			try {
+				InetAddress[] addresses = InetAddress.getAllByName(line); 
+				for(int i = 0; i < addresses.length; i++) {
+					System.out.println(addresses[i].getHostName() + " : " + addresses[i].getHostAddress());
+				}
+				
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+		}
+		sc.close();
 	}
-
 }
